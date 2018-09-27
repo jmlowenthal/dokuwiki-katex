1c189f713db31376c58625e19aa549bb3825ac5a
symera <symera@users.noreply.github.com>
2018-09-27 13:25:04 +0200

    Include CDN/offline setup
    If CDN is checked, load CDN which is stored in configuration.
    If CDN is not checked, load offline KaTeX-library (located in /lib/plugins/katex/lib/katex/...)

--- katex/action.php
+++ katex/action.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * DokuWiki Plugin KaTeX renderer (Action Component)
+ */
 
+// must be run within Dokuwiki
 if (!defined("DOKU_INC")) die();
+
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+define('DOKU_PLUGIN_KATEX',DOKU_BASE.'lib/plugins/'.basename(dirname(__FILE__)).'/');
 
 class action_plugin_katex extends DokuWiki_Action_Plugin {
 	public function register(Doku_Event_Handler $controller) {
@@ -8,20 +15,38 @@ class action_plugin_katex extends DokuWiki_Action_Plugin {
 	}
 	
 	public function handle_tpl_metaheader_output(Doku_Event &$event, $param){
-		// Main CSS
-		$event->data["link"][] = array(
-			"type"		=> "text/css",
-			"rel"		=> "stylesheet",
-			"href"		=> "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"
-		);
-		
-		// Main JS
-		$event->data["script"][] = array(
-			"type"		=> "text/javascript",
-			"_data"		=> "",
-			"src"		=> "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"
-		);
-		
+		if ($this->getConf('cdn')){
+			$cdnlink = $this -> getConf('cdnlink');
+
+			// Main CSS
+			$event->data["link"][] = array(
+				"type"		=> "text/css",
+				"rel"		=> "stylesheet",
+				"href"		=> $cdnlink."katex.min.css"
+			);
+
+			// Main JS
+			$event->data["script"][] = array(
+				"type"		=> "text/javascript",
+				"_data"		=> "",
+				"src"		=> $cdnlink."katex.min.js"
+			);
+		} else {
+			// Main CSS
+			$event->data["link"][] = array(
+				"type"		=> "text/css",
+				"rel"		=> "stylesheet",
+				"href"		=> DOKU_PLUGIN_KATEX."lib/katex/katex.min.css"
+			);
+			
+			// Main JS
+			$event->data["script"][] = array(
+				"type"		=> "text/javascript",
+				"_data"		=> "",
+				"src"		=> DOKU_PLUGIN_KATEX."lib/katex/katex.min.js"
+			);
+		}
+
 		// Render all katex elements
 		$event->data["script"][] = array(
 			"type"		=> "text/javascript",
