b968572a7fa11e1357f8a4c4485c581383054595
symera <symera@users.noreply.github.com>
2018-09-27 13:19:27 +0200

    German language file
    For configuration

---
+++ katex/lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * German language file
+ */
+ 
+// for the configuration manager
+$lang['cdn'] = "Soll die Bibliothek aus einem CDN geladen werden? Die Dateien können schneller geladen werden und die Benutzer haben sie möglicherweise bereits in ihrem Cache.";
+$lang['cdnlink'] = "Bitte geben Sie hier den Pfad zum CDN an, wenn Sie ein CDN verwenden möchten (bis zum letzten Schrägstrich, ohne Dateinamen)<br />Falls nötig, siehe hier: https://cdnjs.com/libraries/KaTeX<br />Standardeinstellung: https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/";
